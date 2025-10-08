088b00b9ff0ed9dc521e63e5b285befe5bdbaed5
quangnd <quangnd412000@gmail.com>
2025-10-08 23:33:34 +0700

    get all users, add user, config path

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,18 +1,21 @@
 import {Request, Response} from 'express';
-import {handleCreateUser} from '../services/user.service';
+import {getAllUsers, handleCreateUser} from 'services/user.service';
 
-const getHomePage = (req: Request, res: Response) => {
-  return res.render('home');
+const getHomePage = async (req: Request, res: Response) => {
+  // get users
+  const users = await getAllUsers();
+  return res.render('home', {users});
 };
 
 const getCreateUserPage = (req: Request, res: Response) => {
   return res.render('create-user');
 };
 
-const postCreateUser = (req: Request, res: Response) => {
+const postCreateUser = async (req: Request, res: Response) => {
   const {name, email, address} = req.body;
   // handle create user
-  handleCreateUser(name, email, address);
+  await handleCreateUser(name, email, address);
+
   return res.redirect('/');
 };
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,3 +1,26 @@
-const handleCreateUser = (name: string, email: string, address: string) => {};
+import getConnection from 'config/db';
 
-export {handleCreateUser};
+const handleCreateUser = async (name: string, email: string, address: string) => {
+  const connection = await getConnection();
+  try {
+    const sql = 'INSERT INTO `users`(`name`, `email`, `address`) VALUES (?, ?, ?)';
+    const values = [name, email, address];
+
+    const [result] = await connection.execute(sql, values);
+    return result;
+  } catch (error) {}
+};
+
+const getAllUsers = async () => {
+  const connection = await getConnection();
+  try {
+    const [results] = await connection.query('SELECT * FROM `users`');
+    return results;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+  return 'hello';
+};
+
+export {handleCreateUser, getAllUsers};
